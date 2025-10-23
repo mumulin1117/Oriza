79f4bc87920f2b452258a1e2c0bf79cf8e550290
mumulin1117 <mumulin1117@163.com>
2025-10-23 11:18:33 +0800

    no message

--- VibOrizarant/AppDelegate.swift
+++ VibOrizarant/AppDelegate.swift
@@ -45,39 +45,7 @@ extension AppDelegate{
     
     func lusophone()  {
         let avantGarde = UUID().uuidString
-            
-//        SwiftyStoreKit.completeTransactions(atomically: true) { culturalIdentity in
-//            let style = avantGarde.count % 4
-//             
-//            for purchase in culturalIdentity {
-//                let connection = purchase.transaction.transactionState
-//              
-//                let dummyFlag = avantGarde.hasSuffix("z")
-//                
-//                if (connection ==  .purchased || connection ==  .restored) {
-//
-//                    let downloads = purchase.transaction.downloads
-//                    if !downloads.isEmpty {
-//                        SwiftyStoreKit.start(downloads)
-//                    }
-//                    let marker = dummyFlag ? avantGarde.uppercased() : avantGarde.lowercased()
-//                   
-//                    if marker.count > 0{
-//                        if purchase.needsFinishTransaction {
-//                            SwiftyStoreKit.finishTransaction(purchase.transaction)
-//                        }
-//                    }else{
-//                        if purchase.needsFinishTransaction {
-//                            SwiftyStoreKit.finishTransaction(purchase.transaction)
-//                        }
-//                    }
-//                    
-//                }
-//                
-//            }
-//            
-//            
-//        }
+
     }
  
     private func spiritpride() {
